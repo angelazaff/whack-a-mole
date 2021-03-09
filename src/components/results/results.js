1ef2074f1f6530f916bc3c81170f989db1b7286a
Angela Zafirovska <zangela95@gmail.com>
2021-03-09 23:34:40 +0100

    Fixes

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -3,11 +3,27 @@ import "./results.scss";
 
 function Results(props) {
   let { counterHit, counter, title } = props;
+  let result = counter / counterHit;
   return (
     <div className={"results " + (!title ? "results-end" : "")}>
       {title && <h1>Whack a mole</h1>}
       <p>
         score {counterHit} / {counter}
+        {(result > 2 && !title) &&
+        <span>
+          <br/>
+          Yikes! You can also try hitting the moles you know?
+
+          </span>
+        }
+        {(result <= 2 && !title) &&
+        <span>
+          <br/>
+          Great job! You just lost 15 seconds of your life
+
+        </span>
+        }
+
       </p>
     </div>
   );
